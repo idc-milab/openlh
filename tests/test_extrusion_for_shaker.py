1f5377f50074d5adfbed47ff449c517aef509a1b
Tomer <tomer2911@gmail.com>
2020-03-13 17:51:30 +0200

    new test for the shaker module

--- tests/test_extrusion_for_shaker.py
+++ tests/test_extrusion_for_shaker.py
@@ -1,18 +1,18 @@
 import sys
-
 sys.path.append('pyuf/')
 from uf.wrapper.swift_api import SwiftAPI
 from uf.utils.log import *
 from time import sleep
-
 logger_init(logging.DEBUG)
 global swift
 swift = SwiftAPI()  # default by filters: {'hwid': 'USB VID:PID=2341:0042'}
 sleep(2)
 print("Allowing extrusion")
 swift.send_cmd_sync("M302 S0")
-
+dict_args = {'x':180, 'y':100, 'z':100, 'e':None, 'speed':None}
+dict_args['wait'] = True
 swift.set_position(**dict_args)
-
+swift.set_position(e=0, speed=1500, timeout=30, wait=True)
+swift.set_position(120, 0, 50, speed=1500, timeout=30, wait=True)
 sleep(1)
 swift.close_conn()
